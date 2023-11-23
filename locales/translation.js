967c392e81759080c5b2f142902d93375ac8babc
Roman <romanmalysh@Romans-MacBook-Air.local>
2023-11-23 17:19:27 +0100

    Updates

--- locales/translation.js
+++
@@ -1,3 +0,0 @@
-export * as pl from './mieszkaniePL/locales/pl/translation.json';
-export * as en from './mieszkaniePL/locales/en/translation.json';
-export * as ru from './mieszkaniePL/locales/pl/translation.json';
